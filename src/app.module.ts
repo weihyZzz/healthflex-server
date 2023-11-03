a98b1018b48e2bc72163477999dcd00b6774e3d0
ustbweihy <56748102+weihyZzz@users.noreply.github.com>
2023-11-03 19:36:01 +0800

    feat:使用typeorm创建user表

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '43.143.180.49',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'healthflex',
+      entities: [`${__dirname}/../modules/**/*.entity{.ts,.js}`],
+      logging: true,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
