de563a4ce3a4ca5e06cefa8a9cb9baee15b4ab73
ustbweihy <56748102+weihyZzz@users.noreply.github.com>
2023-11-05 21:06:13 +0800

    feat: 删除，修改用户

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -21,4 +21,18 @@ export class AppController {
   async find(): Promise<User> {
     return await this.userService.find('22820297-e288-42ba-aadf-3a4062c5c3cb');
   }
+
+  @Get('/delete')
+  async del(): Promise<boolean> {
+    return await this.userService.del('e7c3b2da-f36a-4cfc-9094-f1e09527d4c1');
+  }
+  @Get('/update')
+  async update(): Promise<boolean> {
+    return await this.userService.update(
+      '52a53cd2-3672-49e8-bd12-d9597ac5a2e1',
+      {
+        tel: '18342990089',
+      },
+    );
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -17,6 +17,22 @@ export class UserService {
     }
     return false;
   }
+  //   删除用户
+  async del(id: string): Promise<boolean> {
+    const res = await this.UserRepository.delete(id);
+    if (res && res.affected > 0) {
+      return true;
+    }
+    return false;
+  }
+  //   修改用户
+  async update(id: string, entity: DeepPartial<User>): Promise<boolean> {
+    const res = await this.UserRepository.update(id, entity);
+    if (res.affected > 0) {
+      return true;
+    }
+    return false;
+  }
   //   查找用户
   async find(id: string): Promise<User> {
     const res = await this.UserRepository.findOne({
